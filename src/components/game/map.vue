ad3153aeab723a09d7b64290af2f541a3e1ffea0
fanchaowei <597064177@qq.com>
2023-10-26 20:03:41 +0800

    feat: 地图页面

---
+++ src/components/game/map.vue
@@ -0,0 +1,31 @@
+<script lang='ts'>
+import { defineComponent } from 'vue'
+import floorImg from '@/assets/floor.png'
+import wallImg from '@/assets/wall.png'
+import { useMapStore, MapTile } from '@/store/map.ts'
+
+export default defineComponent({
+name: 'Map'
+})
+</script>
+
+<script setup lang='ts'>
+const { map } = useMapStore()
+
+</script>
+
+<template>
+<div v-for="(_, i) in map" class="flex">
+  <div v-for="(_, j) in map[i]">
+    <template v-if="map[i][j] === MapTile.WALL">
+      <img :src="wallImg" alt="wall" /> 
+    </template>
+    <template v-else-if="map[i][j] === MapTile.FLOOR">
+      <img :src="floorImg" alt="floor">
+    </template>
+  </div>
+</div>
+</template>
+
+<style scoped lang='css'>
+</style>
