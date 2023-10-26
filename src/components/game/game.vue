9b33c4ba79a6338bebf70bee6328b97cd7346f1b
fanchaowei <597064177@qq.com>
2023-10-26 20:03:35 +0800

    feat: 游戏界面页面

---
+++ src/components/game/game.vue
@@ -0,0 +1,22 @@
+<script lang='ts'>
+import { defineComponent } from 'vue'
+export default defineComponent({
+name: 'Game'
+})
+</script>
+
+<script setup lang='ts'>
+import Map from './map.vue'
+import Player from './player.vue'
+
+</script>
+
+<template>
+  <div>
+    <Map></Map>
+    <Player></Player>
+  </div>
+</template>
+
+<style scoped lang='css'>
+</style>
