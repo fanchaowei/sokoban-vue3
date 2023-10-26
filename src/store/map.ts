94c5346aefa819b05245366171990e235af71d38
fanchaowei <597064177@qq.com>
2023-10-26 20:04:05 +0800

    feat: mapStore

---
+++ src/store/map.ts
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+
+export const enum MapTile {
+  WALL = 1,
+  FLOOR = 2
+}
+
+export const useMapStore = defineStore('map', () => {
+  const map = [
+    [1,1,1,1,1],
+    [1,2,2,2,1],
+    [1,2,2,2,1],
+    [1,2,2,2,1],
+    [1,1,1,1,1],
+  ]
+
+  return {
+    map
+  }
+})
