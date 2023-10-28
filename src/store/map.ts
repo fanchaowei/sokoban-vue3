9afe0694ff9d527426f4a22dbbed3e8179aa7a1d
fanchaowei <597064177@qq.com>
2023-10-28 17:14:41 +0800

    feat: 添加 setupMap 功能&测试 setupMap 功能

--- src/store/map.ts
+++ src/store/map.ts
@@ -1,5 +1,9 @@
 import { defineStore } from 'pinia'
 
+import type { IPosition } from '@/types'
+
+type IMap = MapTile[][]
+
 export const enum MapTile {
   WALL = 1,
   FLOOR = 2
@@ -7,14 +11,24 @@ export const enum MapTile {
 
 export const useMapStore = defineStore('map', () => {
   const map = [
-    [1,1,1,1,1],
-    [1,2,2,2,1],
-    [1,2,2,2,1],
-    [1,2,2,2,1],
-    [1,1,1,1,1],
+    [1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 1],
+    [1, 2, 2, 2, 1],
+    [1, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1],
   ]
 
+  function setupMap(newMap: IMap) {
+    map.splice(0, map.length, ...newMap)
+  }
+
+  function isMoveToWall(position: IPosition) {
+    return map[position.y][position.x] === MapTile.WALL
+  }
+
   return {
-    map
+    map,
+    setupMap,
+    isMoveToWall
   }
 })
--- src/store/test/map.spec.ts
+++ src/store/test/map.spec.ts
@@ -1,7 +1,26 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
+import { useMapStore } from '../map'
+import { setActivePinia, createPinia } from 'pinia'
 
 describe('map', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
   it('happy path', () => {
     expect(true).toBe(true)
+  })
+
+  it('调用 setupMap 则改变地图', () => {
+    const { setupMap, map } = useMapStore()
+
+    const newMap = [
+      [2, 2, 2],
+      [2, 2, 2],
+      [2, 2, 2],
+    ]
+
+    setupMap(newMap)
+    expect(map).toEqual(newMap)
   })
 })
