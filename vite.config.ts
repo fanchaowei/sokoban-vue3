c4f709a097a903286fadc463b5a675b530a88fd7
fanchaowei <597064177@qq.com>
2023-11-12 16:40:06 +0800

    fix: 修复 vitest debug 无法正确读取别名路径位置的问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': '/src'
+      '@': `/${path.resolve(__dirname, 'src')}`
     }
   }
 })
