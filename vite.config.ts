b9d6f04ebecdc196636e63dbea706990eea8ef06
fanchaowei <597064177@qq.com>
2025-03-25 19:17:12 +0800

    feat: 使用 pnpm 代替 yarn & 依赖版本更新

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,19 @@ import path from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
-    environment: "happy-dom",
+    environment: 'happy-dom',
   },
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': `/${path.resolve(__dirname, 'src')}`
-    }
-  }
+      '@': `/${path.resolve(__dirname, 'src')}`,
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/style/index.scss";',
+      },
+    },
+  },
 })
